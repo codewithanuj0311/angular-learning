05bce3d07bb648328e1c24ef53507abcac95aafc
Anuj Raj <anuj.dev0311@gmail.com>
2026-08-18 20:29:35 +0530

    added ts for comp3

---
+++ src/app/comp3/comp3.component.ts
@@ -0,0 +1,22 @@
+import { DesignUtilityService } from './../appServices/design-utility.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-comp3',
+  templateUrl: './comp3.component.html',
+  styleUrls: ['./comp3.component.css']
+})
+export class Comp3Component implements OnInit {
+
+  constructor(private designService: DesignUtilityService) {
+    this.designService.username.subscribe((uname)=> {
+      this.username = uname;
+    })
+   }
+
+  username:any;
+
+  ngOnInit(): void {
+  }
+
+}
